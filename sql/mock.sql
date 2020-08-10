a9591732f16868d2fe80697ecca72bb02642d6ce
mebird <mbe_pro@outlook.com>
2020-08-09 22:13:24 -0700

    fixing up mock data

--- sql/mock.sql
+++ sql/mock.sql
@@ -1,10 +1,10 @@
 INSERT INTO courses (pl_course_id)
 VALUES (1);
 INSERT INTO students (course_id, discord_id)
-VALUES (0, 'moonbird#7618');
-INSERT INTO integration (integration_id)
+VALUES (1, 'moonbird#7618');
+INSERT INTO integrations (integration_id)
 VALUES ('DISCORD'),
     ('PL'),
     ('PIAZZA');
 INSERT INTO points (student_id, integration_id)
-VALUES (0, 'DISCORD');
+VALUES (1, 'DISCORD');
